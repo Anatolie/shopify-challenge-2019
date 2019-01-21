c2a63594dd3de0b7af2cb1a9e9c89c671d8a164e
Anatolie Diordita <contact@anatolie.me>
2019-01-20 23:07:41 -0500

    Create search results component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,16 +2,19 @@
   <div>
     <h1>Toronto Waste Lookup</h1>
     <search-form />
+    <search-results />
   </div>
 </template>
 
 <script>
 import searchForm from './search/form'
+import searchResults from './search/results'
 
 export default {
   name: 'shopify-challenge-2019',
   components: {
-    searchForm
+    searchForm,
+    searchResults
   }
 }
 </script>
---
+++ src/components/search/results.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="search-results">
+
+  </section>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      results: []
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.search-results {
+
+}
+</style>
