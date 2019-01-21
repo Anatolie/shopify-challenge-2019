a44ba7c1e9732998f8a491f5473878d05f0da9f7
Anatolie Diordita <contact@anatolie.me>
2019-01-20 23:00:04 -0500

    Create search form component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
     <h1>Toronto Waste Lookup</h1>
+    <search-form />
   </div>
 </template>
 
 <script>
+import searchForm from './search/form'
+
 export default {
-  name: 'shopify-challenge-2019'
+  name: 'shopify-challenge-2019',
+  components: {
+    searchForm
+  }
 }
 </script>
 
---
+++ src/components/search/form.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="search-form">
+    <form @submit.prevent.stop="submit">
+      <div class="search-input">
+        <input v-model="query" name="query" type="text" placeholder="Type here to search" />
+        <button type="submit">Search</button>
+      </div>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      query: null
+    }
+  },
+  methods: {
+    async submit () {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.search-form {
+
+}
+</style>
