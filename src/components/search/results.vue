1ff65c343660037cbd48a4431d81bb371324346b
Anatolie Diordita <contact@anatolie.me>
2019-01-21 01:24:49 -0500

    Add favourite button into results list

--- src/components/search/results.vue
+++ src/components/search/results.vue
@@ -1,6 +1,11 @@
 <template>
   <section class="search-results">
     <article v-for="result in results" :key="result.id" class="result">
+      <div class="favourite">
+        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512">
+          <path d="M512 198.525l-176.89-25.704-79.11-160.291-79.108 160.291-176.892 25.704 128 124.769-30.216 176.176 158.216-83.179 158.216 83.179-30.217-176.176 128.001-124.769z"></path>
+        </svg>
+      </div>
       <div class="title">
         <h3>{{ result.title }}</h3>
       </div>
@@ -28,8 +33,18 @@ export default {
   display: flex;
   margin-bottom: 32px;
 
-  &:last-child {
-    margin-bottom: 0;
+  .favourite {
+    width: 20px;
+    height: 20px;
+    margin-right: 12px;
+    display: block;
+    cursor: pointer;
+
+    svg {
+      width: 20px;
+      height: 20px;
+      fill: #aaa;
+    }
   }
   .title {
     flex: 1;
@@ -40,6 +55,9 @@ export default {
   }
   .body {
     flex: 1;
+  }
+  &:last-child {
+    margin-bottom: 0;
   }
 }
 </style>
