d4e97d5aa4af6cd0b4f7d3a6e26401ef14b37d36
Anatolie Diordita <contact@anatolie.me>
2019-01-20 22:37:33 -0500

    Remove default webpack content

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Toronto Waste Lookup</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'shopify-challenge-2019'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
